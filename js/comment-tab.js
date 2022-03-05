ab0c986a6bb22427cd06ea378b9f26ee5cbe5c81
KIMSANGYEOB <sykim10070@gmail.com>
2022-03-06 02:23:47 +0900

    tistory 커스터마이징 용 html, css, js 작성

---
+++ js/comment-tab.js
@@ -0,0 +1,8 @@
+$('.tab-button').on('click', function (e) {
+  $('.tab-button').removeClass('active');
+  $(this).addClass('active');
+
+  $('.tab-content').removeClass('active');
+  const target = $(this).data('id');
+  $(`#${target}`).addClass('active');
+});
